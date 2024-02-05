730461f2d19cb18e4b79597ab3fd4f0824649e20
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-02-05 19:31:35 +0200

    hw1

---
+++ third semester/main.js
@@ -0,0 +1,37 @@
+let age = 16;
+console.log(age);
+
+let name = "Danya";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "критикуєшь - пропонуй!";
+console.log(myString);
+
+let myNumber = 7;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+let nameInput = prompt("Ваше ім'я");
+alert(nameInput);
+
+const acceptMenu = confirm("закрити меню?");
+if (acceptMenu == true) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+let warning = alert("Дія небезпечна");
+console.log(warning)
+
+const warningAccept = confirm("закрити меню?");
+if (warningAccept == true) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
