5e49a8a318963ac47ea33a7bf6cf76f39c4bdd87
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-02-23 12:47:36 +0200

    fix hw

--- third semester/hw2/main.js
+++ third semester/hw2/main.js
@@ -1,4 +1,4 @@
-let temperatureCelsius = Math.round(Math.random() * (100 + 100) - 100);
+let temperatureCelsius = Math.round(Math.random() * (100 - 100) + 100);
 const temperatureFahrenheit = (temperatureCelsius * 9) / 5 + 32;
 console.log(temperatureFahrenheit);
 
@@ -12,32 +12,31 @@ console.log(
 
 let healthPoint = 100;
 let energyPoint = 100;
-const energySprint = Math.round(Math.random() * (11 + 0) - 0);
-console.log(healthPoint - Math.round(Math.random() * (101 + 0) - 0) + "+");
+const energySprint = Math.round(Math.random() * (11 - 0) + 0);
+console.log(healthPoint - Math.round(Math.random() * (101 - 0) + 0) + "+");
 console.log(energyPoint - energySprint + "⚡");
 
-let priceOfPhone = 500 + "$";
+let priceOfPhone = Math.round(Math.random() * (1100 - 150) + 150);
 let discount = 10;
-const priceAfterDiscount = (parseInt(priceOfPhone) / 100) * (100 - discount);
+const priceAfterDiscount = Math.ceil((priceOfPhone / 100) * (100 - discount));
 console.log(priceAfterDiscount + "$");
 
-let randomNumber = Math.random() * (101 + 0) - 0;
+let randomNumber = Math.random() * (101 - 0) + 0;
 console.log(Math.floor(randomNumber));
 
-// const task6 = toString(Math.random() * (11 + 0) - 0);
-const task6 = "17.45kfkewhfk";
-console.log(parseFloat(task6));
+const numFloat = "17.45kfkewhfk";
+console.log(parseFloat(numFloat));
 
-const task7 = "17.45kfkewhfk";
-console.log(parseInt(task7));
+const numInt = "17.45kfkewhfk";
+console.log(parseInt(numInt));
 
-let numberForSqrt = Math.floor(Math.random() * (501 + 0) - 0);
+let numberForSqrt = Math.floor(Math.random() * (501 - 0) + 0);
 numberForSqrt = Math.sqrt(numberForSqrt);
 console.log(numberForSqrt);
 
-let numberSrting = (Math.random() * (101 + 0) - 0).toString();
-let numberNumber = Math.random() * (101 + 0) - 0;
+let numberSrting = (Math.random() * (101 - 0) + 0).toString();
+let numberNumber = Math.random() * (101 - 0) + 0;
 numberSrting = parseInt(numberSrting);
-numberNumber= numberNumber.toString();
-console.log(numberSrting , typeof numberSrting);
-console.log(numberNumber , typeof numberNumber);
+numberNumber = numberNumber.toString();
+console.log(numberSrting, typeof numberSrting);
+console.log(numberNumber, typeof numberNumber);
