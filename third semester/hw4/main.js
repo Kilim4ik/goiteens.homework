f7d041d06b03f2d7f5e08b8fbfdcfff7bbda61d8
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-03-12 09:35:07 +0200

    fix validation

--- third semester/hw4/main.js
+++ third semester/hw4/main.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const shortName = prompt("ur short name");
-const fullName = prompt("ur full name");
-if (shortName != null && fullName != null) {
-  alert("Обидва поля заповнені");
-} else {
-  alert("Не всі поля заповненi");
-}
+// const shortName = prompt("ur short name");
+// const fullName = prompt("ur full name");
+// if (shortName != null && fullName != null) {
+//   alert("Обидва поля заповнені");
+// } else {
+//   alert("Не всі поля заповненi");
+// }
 
 // const fstNum = prompt("Перше число");
 // const sndNum = prompt("Друге число");
@@ -21,40 +21,44 @@ if (shortName != null && fullName != null) {
 //   alert("Ти бомбом? Треба було числа");
 // }
 
-const fstNum = Number(prompt("Перше число"));
-const sndNum = Number(prompt("Друге число"));
-if (isNaN(fstNum) || isNaN(sndNum)) {
-  alert("Ти бомбом? Треба було числа");
-} else if (fstNum + sndNum > 10) {
-  alert("Сума більша за 10");
-} else {
-  alert("Сума менша або дорівнює 10");
-}
+// const fstNum = Number(prompt("Перше число"));
+// const sndNum = Number(prompt("Друге число"));
+// if (isNaN(fstNum) || isNaN(sndNum)) {
+//   alert("Ти бомбом? Треба було числа");
+// } else if (fstNum + sndNum > 10) {
+//   alert("Сума більша за 10");
+// } else {
+//   alert("Сума менша або дорівнює 10");
+// }
 
-let userInput = prompt("Введіть текст...");
-userInput = userInput.trim().toLocaleLowerCase();
-if (userInput == "javascript") {
-  alert("Текст містить слово JavaScript");
-} else {
-  alert("Текст не містить слово JavaScript");
-}
+// let userInput = prompt("Введіть текст...");
+// userInput = userInput.trim().toLocaleLowerCase();
+// if (userInput.includes('javascript')) {
+//   alert("Текст містить слово JavaScript");
+// } else {
+//   alert("Текст не містить слово JavaScript");
+// }
 
 const num = Number(prompt("enter number ... "));
-if (!isNaN(num) && num >= 10 && num <= 20) {
+if (isNaN(num)) {
+  alert("Неправильний формат");
+  
+}else if( num >= 10 && num <= 20){
   alert("Число входить в діапазон від 10 до 20");
-} else {
+}
+else {
   alert("Число не входить в діапазон від 10 до 20");
 }
 
-const userName = prompt("ur name");
-const userMail = prompt("email");
-const userPassword = prompt("password");
-if (
-  userName.length >= 3 &&
-  userMail.includes("@") &&
-  userPassword.length >= 6
-) {
-  alert("Коректно заповнені дані");
-} else {
-  alert("fail");
-}
+// const userName = prompt("ur name");
+// const userMail = prompt("email");
+// const userPassword = prompt("password");
+// if (
+//   userName.length >= 3 &&
+//   userMail.includes("@") &&
+//   userPassword.length >= 6
+// ) {
+//   alert("Коректно заповнені дані");
+// } else {
+//   alert("fail");
+// }
