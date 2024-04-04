895620316ea6fcf7d26edd724e9933ca0bede0d8
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-04-04 14:32:57 +0300

    add last task

--- third semester/hw8/main.js
+++ third semester/hw8/main.js
@@ -1,14 +1,14 @@
 "use srtict";
 
-const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+// const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
 
-let string = "";
-for (let i = 0; i < friends.length; i++) {
-  string = string + "," + friends[i];
-}
-console.log(string);
-string = friends.join();
-console.log(string);
+// let string = "";
+// for (let i = 0; i < friends.length; i++) {
+//   string = string + "," + friends[i];
+// }
+// console.log(string);
+// string = friends.join();
+// console.log(string);
 
 const cards = [
   "Карточка-1",
@@ -21,8 +21,10 @@ const caratoRemane = "Карточка-3";
 const cardIaInsert = "Карточка-6";
 const cardIoUpdate = "Карточка-4";
 cards.splice(cards.indexOf(caratoRemane), 1);
-
 console.log(cards);
+
 cards.splice(cards.length, 0, cardIaInsert);
 console.log(cards);
-//що саме треба оновити в останьому завданні ? 
+
+cards.splice(cards.indexOf(cardIoUpdate), 1, "Карточка-нова");
+console.log(cards);
