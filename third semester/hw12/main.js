e9933a4d2955cb7e7e3b83c1ba8bfbe6bcdb0a91
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-05-05 20:59:04 +0300

    hw12

---
+++ third semester/hw12/main.js
@@ -0,0 +1,193 @@
+"use strict";
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+
+const calcUserStat = (obj) => {
+  const userKeys = Object.keys(user);
+  const newArr = [];
+  for (let key of userKeys) {
+    newArr.push(key);
+  }
+  return newArr;
+};
+
+const showUserStat = (obj, callbak) => {
+  for (let elem of callbak) {
+    console.log(`${elem} : ${user[elem]}`);
+  }
+};
+// ! showUserStat(user, calcUserStat(user));
+
+const countProps = (obj) => Object.entries(obj).length;
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
+
+const findBestEmployee = (employees) => {
+  let bestEmploee = Object.entries(employees)[0];
+  for (let employee of Object.entries(employees)) {
+    if (employee[1] > bestEmploee[1]) {
+      bestEmploee = employee;
+    }
+  }
+  return bestEmploee;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
+
+const countTotalSalary = (employees) => {
+  const arrSalary = Object.values(employees);
+  let total = 0;
+  for (let num of arrSalary) {
+    total += num;
+  }
+  return total;
+};
+
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроїд", price: 400, quantity: 7 },
+  { name: "Захоплення", price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = (arr, prop) => {
+  const newArr = [];
+  for (let elem of arr) {
+    newArr.push(elem[prop]);
+  }
+  return newArr;
+};
+
+
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроїд', 'Захоплення']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
+
+const calculateTotalPrice = function (allProdcuts, productName) {
+  for (let product of allProdcuts) {
+    if (product["name"] == productName) {
+      return product["price"] * product["quantity"];
+    }
+  }
+};
+
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроїд"));
+
+
+
+const Transaction = {
+    DEPOSIT: 'deposit',
+    WITHDRAW: 'withdraw',
+  };
+  
+  /*
+   * Кожна транзакція - це об'єкт з властивостями: id, type і amount
+   */
+  
+  const account = {
+    // Поточний баланс рахунку
+    balance: 0,
+  
+    // Історія транзакцій
+    transactions: [],
+  
+    /*
+     * Метод створює і повертає об'єкт транзакції.
+     * Приймає суму і тип транзакції.
+     */
+    createTransaction(amount, type) {},
+  
+    /*
+     * Метод відповідає за додавання суми до балансу.
+     * Приймає суму танзакції.
+     * Викликає createTransaction для створення об'єкта транзакції
+     * після чого додає його в історію транзакцій
+     */
+    deposit(amount) {},
+  
+    /*
+     * Метод відповідає за зняття суми з балансу.
+     * Приймає суму танзакції.
+     * Викликає createTransaction для створення об'єкта транзакції
+     * після чого додає його в історію транзакцій.
+     *
+     * Якщо amount більше, ніж поточний баланс, виводь повідомлення
+     * про те, що зняття такої суми не можливо, недостатньо коштів.
+     */
+    withdraw(amount) {},
+  
+    /*
+     * Метод повертає поточний баланс
+     */
+    getBalance() {},
+  
+    /*
+     * Метод шукає і повертає об'єкт транзакції по id
+     */
+    getTransactionDetails(id) {},
+  
+    /*
+     * Метод повертає кількість коштів
+     * певного типу транзакції з усієї історії транзакцій
+     */
+    getTransactionTotal(type) {},
+  };
