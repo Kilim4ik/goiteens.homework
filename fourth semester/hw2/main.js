2439feaa09309f6c16059ff3d98554d7b18a5e30
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-08-21 12:42:03 +0300

    hw2

---
+++ fourth semester/hw2/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const input = document.querySelector(".slider__input");
+const img = document.querySelector(".slider__image");
+
+input.addEventListener(
+  "input",
+  _.debounce((e) => {
+    const val = e.target.value;
+    if (val > 50)
+      img.style.transform = `scale(${1 + (val - 50) / 100}
+    )`;
+    else if (val < 50)
+      img.style.transform = `scale(${val / 100}
+    )`;
+    else img.style.transform = "scale(1)";
+  }, 1000)
+);
+
+const box = document.querySelector("#box");
+document.addEventListener(
+  "mousemove",
+  _.debounce((e) => {
+    box.style.top = `${e.clientY}px`;
+    box.style.left = `${e.clientX}px`;
+  }, 100)
+);
