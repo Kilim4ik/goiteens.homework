9e770f1df827d4ab01ae3ce8c3e991be6caf4f0b
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-09-10 16:36:44 +0300

    test commit

--- fourth semester/hw4/main.js
+++ fourth semester/hw4/main.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector("button");
 btn.addEventListener("click", (e) => {
+  //test
   //   console.log(document.querySelector("#a").value);
   console.log(1);
   localStorage.setItem(
