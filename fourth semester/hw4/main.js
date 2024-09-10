dba9c92c3cb5e12a6675ac2d036df696c753cd2d
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-09-10 16:34:23 +0300

    add hw4

---
+++ fourth semester/hw4/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("button");
+btn.addEventListener("click", (e) => {
+  //   console.log(document.querySelector("#a").value);
+  console.log(1);
+  localStorage.setItem(
+    document.querySelector("#a").value,
+    document.querySelector("#b").value
+  );
+});
