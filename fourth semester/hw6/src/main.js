bbe73273b941494a8189c08d41e80956ca01db14
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-09-26 20:52:44 +0300

    add hw6

--- fourth semester/hw6/src/main.js
+++ fourth semester/hw6/src/main.js
@@ -1,5 +1,20 @@
 "use strcict";
+import { historyArray, pushToHistory } from "./history";
+import { success } from "../node_modules/@pnotify/core/dist/PNotify.js";
+import "@pnotify/core/dist/PNotify.css";
+
 const form = document.querySelector("form");
+const arrayOfInputs = document.querySelectorAll("input");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  pushToHistory({
+    name: arrayOfInputs[0].value,
+    email: arrayOfInputs[1].value,
+    message: arrayOfInputs[2].value,
+  });
+  arrayOfInputs.forEach((input) => (input.value = ""));
+  success({
+    title: "Success!",
+    text: "Message was sended!",
+  });
 });
