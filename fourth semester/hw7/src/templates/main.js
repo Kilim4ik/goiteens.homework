5715fafac1a4e649b0db29e15b63880554ba9387
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-09-29 14:04:55 +0300

    hw 7
    don't working handlebars func is str

---
+++ fourth semester/hw7/src/templates/main.js
@@ -0,0 +1,66 @@
+"use strict";
+const users = [
+  {
+    name: "Emily",
+    lastName: " Johnson",
+    age: 28,
+    photo: null,
+  },
+  {
+    name: " Michael",
+    lastName: " Smith",
+    age: 34,
+    photo: null,
+  },
+  {
+    name: "Sarah",
+    lastName: "Williams",
+    age: 25,
+    photo: null,
+  },
+  {
+    name: "David",
+    lastName: "Brown",
+    age: 42,
+    photo: null,
+  },
+  {
+    name: "Olivia",
+    lastName: "Taylor",
+    age: 31,
+    photo: null,
+  },
+  {
+    name: "James",
+    lastName: "Miller",
+    age: 29,
+    photo: null,
+  },
+  {
+    name: "Sophia",
+    lastName: "Davis",
+    age: 23,
+    photo: null,
+  },
+  {
+    name: "Benjamin",
+    lastName: "Wilson",
+    age: 36,
+    photo: null,
+  },
+  {
+    name: "Danylo",
+    lastName: "Nutella",
+    age: 24,
+    photo: null,
+  },
+];
+import menuTemplate from "./template.hbs";
+
+console.log(typeof menuTemplate);
+// const userTemblate = {
+//   title: "our users",
+//   users: users,
+// };
+// const markup = menuTemplate(userTemblate);
+// document.querySelector(".users-section").innerHTML = markup;
