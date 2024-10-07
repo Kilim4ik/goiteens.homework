54a357bb9a6e9c2b92d3b428377a29a57afef416
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-10-07 14:00:57 +0300

    fix comments

--- fourth semester/hw4/main.js
+++ fourth semester/hw4/main.js
@@ -1,11 +1,12 @@
-import validator from "validator";
-const btn = document.querySelector("button");
+const btn = document.querySelector("#continue-button");
+const greet = document.querySelector(".user-name");
 btn.addEventListener("click", (e) => {
-  //test
-  //   console.log(document.querySelector("#a").value);
   console.log(1);
   localStorage.setItem(
     document.querySelector("#a").value,
     document.querySelector("#b").value
   );
+  greet.textContent = ` Вітаємо , ${localStorage.getItem(
+    document.querySelector("#a").value
+  )}`;
 });
